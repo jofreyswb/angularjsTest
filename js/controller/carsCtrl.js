4e318c7b4bbb67f62b4e26b80de6e66d55d39643
jofreyswb <jofreyswb@gmail.com>
2016-02-17 08:19:31 +0200

    бля не робить

--- js/controller/carsCtrl.js
+++ js/controller/carsCtrl.js
@@ -1,7 +1,14 @@
 
     var carsAppControllers =  angular.module('carsAppControllers',[]);
 
-    carsAppControllers.controller('carsCtrl', ['$scope', 'Car',
+    carsAppControllers.controller('carsCtrl', ['$scope','$routeParams', 'Car',
         function ($scope, Car){
-
+             $scope.cars = Car.query();
+             var car = Car.query(function(){
+                 console.log(car);
+             });
+             var entry = Car.get({ id: $scope.id }, function() {
+                 console.log(entry);
+             })
+            console.log('hello');
         }]);
--- js/services/services.js
+++ js/services/services.js
@@ -2,7 +2,5 @@
 
     carServices.factory('Car',['$resource',
             function($resource){
-                return $resource('phones/:phoneId.json', {}, {
-                    query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
-                });
+                return $resource('/api/cars/:id');
     }]);
