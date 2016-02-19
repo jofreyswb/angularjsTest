62ad93ccd877c4c669f717c3a82313b99aae1180
jofreyswb <jofreyswb@gmail.com>
2016-02-19 09:00:52 +0200

    Create and view

--- js/controller/carsCtrl.js
+++ js/controller/carsCtrl.js
@@ -3,12 +3,16 @@
 
     carsAppControllers.controller('carsCtrl', ['$scope','$routeParams', 'Car',
         function ($scope, $routeParams, Car){
+
              var car = Car.query(function(){
                  $scope.carbrends = car;
 
              });
 
+            $scope.Edit = function(){};
+
             $scope.carbrand = Car.get({ id: $routeParams.carbrandId });
+
 
             $scope.Delete = (function(carbrandId) {
                 Car.delete({ id:carbrandId });
